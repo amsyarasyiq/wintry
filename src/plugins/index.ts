e32643eca89e3a94d975947530b4b035541034c2
pylixonly <82711525+pylixonly@users.noreply.github.com>
2024-12-06 21:30:01 +0800

    feat(experiments): add experiments core plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,10 @@
 import { StartAt, type WintryPlugin } from "./types";
 
-const Plugins: Array<WintryPlugin> = [require("./_core/settings").default, require("./_core/no-track").default];
+const Plugins: Array<WintryPlugin> = [
+    require("./_core/settings").default,
+    require("./_core/no-track").default,
+    require("./_core/experiments").default,
+];
 
 export function startAllPlugins(stage: StartAt) {
     for (const plugin of Plugins) {
