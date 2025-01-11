112e5efa2f64de9d67ebb04e4eab03fe367272b3
pylixonly <82711525+pylixonly@users.noreply.github.com>
2025-01-12 00:08:40 +0800

    feat: Add patch to prevent moment locale changes during module registration

--- src/metro/internal/index.ts
+++ src/metro/internal/index.ts
@@ -1,4 +1,4 @@
-import { before } from "../../patcher";
+import { before, instead } from "../../patcher";
 import { markExportsFlags, setupMetroCache } from "./caches";
 import { _importingModuleId, moduleRegistry, patchModule } from "./modules";
 
@@ -31,12 +31,25 @@ export function initializeMetro() {
     );
 
     // Essential patch for brute finding modules, so (bad) modules that aren't supposed to be initialized
-    // won't make the app stop working
+    // won't make the app stop working/working oddly
 
     // Fix UI thread hanging
     patchModule(
         exp => exp.default?.reactProfilingEnabled,
         // The bad module is next to the module that is being checked
         ({ id }) => markExportsFlags(id + 1, undefined),
+    );
+
+    // Prevent modules that registers a moment locale from changing the current locale
+    // Not tested, directly ported from Bunny's code, but it should work
+    patchModule(
+        exp => exp.isMoment,
+        ({ module: { exports } }) => {
+            instead(exports, "defineLocale", (args, orig) => {
+                const origLocale = exports.locale();
+                orig(...args);
+                exports.locale(origLocale);
+            });
+        },
     );
 }
