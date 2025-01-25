2f35c13878b4f2293675daeffc807a03176ce9e0
pylixonly <82711525+pylixonly@users.noreply.github.com>
2025-01-25 11:43:35 +0800

    feat(ui): Add little border for plugin settings

--- src/components/WintrySettings/pages/Plugins/options/OptionSection.tsx
+++ src/components/WintrySettings/pages/Plugins/options/OptionSection.tsx
@@ -1,5 +1,5 @@
+import { View } from "react-native";
 import { t } from "../../../../../i18n";
-import { TableRowGroup } from "../../../../../metro/common";
 import type { WintryPluginInstance } from "../../../../../plugins/types";
 import { getPluginSettings } from "../../../../../plugins/utils";
 import { InfoCard } from "../PluginSheetComponent";
@@ -11,11 +11,12 @@ export function OptionSection({ plugin }: { plugin: WintryPluginInstance }) {
 
     return (
         <InfoCard label={t.settings.plugins.info_sheet.configurations()}>
-            <TableRowGroup>
+            {/* TODO: There have to be a better way to do this */}
+            <View style={{ borderWidth: 0.9, borderColor: "rgba(0, 0, 0, 0.3)", borderRadius: 12, overflow: "hidden" }}>
                 {Object.entries(options).map(([key, def], i) => {
                     return <OptionDefRow key={i} settingKey={key} opt={def} plugin={plugin} />;
                 })}
-            </TableRowGroup>
+            </View>
         </InfoCard>
     );
 }
