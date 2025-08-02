c5fdae2033a16ad2f85a1723ab00f4d19c1731f4
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2025-08-03 02:23:55 +0800

    feat(plugins): add command to toggle silent typing mode (non-functional)

--- src/plugins/silent-typing/index.ts
+++ src/plugins/silent-typing/index.ts
@@ -1,4 +1,6 @@
 import { definePlugin, definePluginSettings } from "#plugin-context";
+import { defineCommand, replyCommand } from "@api/commands/helpers";
+import { ApplicationCommandOptionType } from "@api/commands/types";
 import { showToast } from "@api/toasts";
 import { Devs } from "@data/constants";
 
@@ -15,11 +17,34 @@ const settings = definePluginSettings({
     },
 });
 
-// TODO: Easy toggle with command to enable/disable silent typing
 export default definePlugin({
     name: "SilentTyping",
     description: "Conceal your typing status to ensure your activity remains private while writing messages.",
     authors: [Devs.Pylix],
+
+    commands: [
+        defineCommand({
+            name: "silent-typing",
+            description: "Toggle silent typing mode.",
+            options: [
+                {
+                    name: "enable",
+                    type: ApplicationCommandOptionType.BOOLEAN,
+                    description: "Enable or disable silent typing mode.",
+                    required: true,
+                },
+            ],
+            execute([enable], ctx) {
+                // // TODO: There's currently no way to mutate the store yet, silly.
+                // const current = settings.get().silentTypingActive;
+                // settings.set({ silentTypingActive: enable?.value });
+
+                replyCommand(ctx.channel.id, {
+                    content: `Silent typing mode is now ${enable?.value ? "enabled" : "disabled"}.`,
+                });
+            },
+        }),
+    ],
 
     flux: {
         TYPING_START_LOCAL: () => {
